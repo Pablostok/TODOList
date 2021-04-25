771c4148367258f43ca62db6535fbe515e402743
Pablo <pablovramos.007@gmail.com>
2021-04-25 18:14:29 +0200

    funcion loadData

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,6 +1,34 @@
+var cont = 0;
+
+function loadData(){
+    let finished = false;
+    let contSearch = 0;
+    while(finished == false){
+        var currentItem = localStorage.getItem('tarea-'+contSearch);
+        contSearch++;
+        if(currentItem == null){
+            finished = true;
+            break;
+        }
+        //Creación elemento HTML
+        var div = document.createElement("div");
+        div.classList.add("estilo-item");
+        var paragraph = document.createElement("p");
+
+        div.appendChild(paragraph);
+        paragraph.innerText = currentItem;
+
+        var container = document.querySelector(".Container");
+        container.appendChild(div);
+    }
+}
+
 function addItem(){
     var textoFormulario = document.querySelector('#tareaARecoger').value;
     console.log(textoFormulario);
+
+    localStorage.setItem('tarea-'+cont, textoFormulario);
+    cont++;
 
     var div = document.createElement("div");
     div.classList.add("estilo-item");
